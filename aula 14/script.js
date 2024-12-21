8bb1c7309d1f98312340cc461459bc87918d3084
Gabzn0 <165189258+Justiceiro92@users.noreply.github.com>
2024-12-21 15:20:21 -0300

    aula 14

---
+++ aula 14/script.js
@@ -0,0 +1,9 @@
+const POKE_NAME = document.querySelector('.poke_name')
+const POKE_NUMBER = document.querySelector('.poke_number')
+const POKE_IMG = document.querySelector('.poke_img')
+const CONTAINER_INPUT = document.querySelector('.container_input')
+const INPUT = document.querySelector('input')
+const PREV_BTN = document.querySelector('.pre_btn')
+const NEXT_BTN = document.querySelector('.next_btn')
+
+let id_pokemon = 1
