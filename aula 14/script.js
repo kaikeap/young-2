a10cc6fe9c67b2729e42b7160b878c40a5eb6b82
Justiceiro92 <165189258+Justiceiro92@users.noreply.github.com>
2025-02-01 15:30:01 -0300

    aula 14

--- aula 14/script.js
+++ aula 14/script.js
@@ -5,5 +5,47 @@ const CONTAINER_INPUT = document.querySelector('.container_input')
 const INPUT = document.querySelector('input')
 const PREV_BTN = document.querySelector('.pre_btn')
 const NEXT_BTN = document.querySelector('.next_btn')
+const URL = 'https://pokeapi.co/api/v2/pokemon/'
 
 let id_pokemon = 1
+
+const buscar_pokemon = async (pokemon) => {
+    let resposta = await fetch(`${URL}${pokemon}`)
+    if (resposta.status === 200) {
+        const INFO = resposta.json()
+        return INFO
+    }
+}
+
+const mostrar_pokemon = async (pokemon) => {
+    const json_pokemon = await buscar_pokemon(pokemon)
+    POKE_NUMBER.innerHTML = json_pokemon.id
+    POKE_NAME.innerHTML = json_pokemon.name
+    POKE_IMG.src = json_pokemon['sprites']['other']['showdown']['front_default']
+    INPUT.value =''
+    id.pokemon = json_pokemon.id
+}
+
+
+CONTAINER_INPUT.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    let pokemon = INPUT.value.toLowerCase()
+    mostrar_pokemon(pokemon)
+})
+
+
+PREV_BTN.addEventListener('click', () => {
+    if(id_pokemon > 1) {
+    id_pokemon -= 1
+    mostrar_pokemon(id_pokemon)
+    }
+})
+
+NEXT_BTN.addEventListener('click', () =>{
+    if (id_pokemon < 920) {
+        id_pokemon += 1
+        mostrar_pokemon(id_pokemon)
+    }
+})
+
+window.onload = mostrar_pokemon(id_pokemon)
